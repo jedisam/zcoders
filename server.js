b98ef519d6545bc58f724fcce04ac6b97e642479
Yididya Samuel <yidisam18@gmail.com>
2020-05-20 03:12:49 -0400

    express-validation

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const app = express();
+const express = require ('express');
+const connectDB = require ('./config/db');
+const app = express ();
 
 // Connect DB
-connectDB();
+connectDB ();
 
-app.get("/", (req, res) => {
-  res.send("API Runing");
+// Init middleware
+app.use ( express.json ({ exteded: false }));
+
+app.get ('/', (req, res) => {
+  res.send ('API Runing');
 });
 
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
+app.use ('/api/users', require ('./routes/api/users'));
+app.use ('/api/auth', require ('./routes/api/auth'));
+app.use ('/api/profile', require ('./routes/api/profile'));
+app.use ('/api/posts', require ('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen (PORT, () => {
+  console.log (`Server started on port ${PORT}`);
 });
