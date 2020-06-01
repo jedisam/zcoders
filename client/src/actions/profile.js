cf9afd01b2532b9e49a8d393d68315cc8e78246d
Yididya Samuel <yidisam18@gmail.com>
2020-06-01 10:58:09 -0400

    profile page

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,5 +7,15 @@ import { GET_PROFILE, PROFILE_ERROR } from "./types";
 export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get("http://localhost:5000/api/profile/me");
-  } catch (err) {}
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status}
+    })
+  }
 };
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,15 +1,29 @@
-const initialState = {
-    profile: null,
-    profiles: [],
-    repos: [],
-    loading: true,
-    error: {}
-}
+import {PROFILE_ERROR, GET_PROFILE} from '../actions/types';
 
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
 
 export default (state = initialState, action) => {
-    const { type, payload } = action;
-    switch(type) {
-        case GET_PROFILE:
-    }
-}
+  const {type, payload} = action;
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
