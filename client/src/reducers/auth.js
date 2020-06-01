9801593a44410954e0f6dffb29f25017c9e64b73
Yididya Samuel <yidisam18@gmail.com>
2020-05-31 23:12:56 -0400

    hidden files

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,22 +6,22 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-} from '../actions/types';
+} from "../actions/types";
 
 const initialState = {
-  token: localStorage.getItem ('item'),
+  token: localStorage.getItem("item"),
   isAuthenticated: null,
   isLoading: true,
   user: null,
 };
 
 export default (state = initialState, action) => {
-  const {type, payload} = action;
+  const { type, payload } = action;
 
   switch (type) {
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
-      localStorage.setItem ('token', payload.token);
+      localStorage.setItem("token", payload.token);
       return {
         ...state,
         ...payload,
@@ -32,7 +32,7 @@ export default (state = initialState, action) => {
     case AUTH_ERROR:
     case LOGIN_FAIL:
     case LOGOUT:
-      localStorage.removeItem ('token');
+      localStorage.removeItem("token");
       return {
         ...state,
         token: null,
