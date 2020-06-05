de2b5c7a745e6b191953b0cc37321571da470cb5
Yididya Samuel <yidisam18@gmail.com>
2020-06-05 01:29:36 -0400

    prepared for deployment

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const app = express();
-
+const path = require("path");
 
 app.use(cors());
-
 
 // Connect DB
 connectDB();
 
-
 // Init middleware
 app.use(express.json({ extended: false }));
-
-
-app.get("/", (req, res) => {
-  res.send("API Runing");
-});
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// serve static asset in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))});
+}
 
 const PORT = process.env.PORT || 5000;
 
